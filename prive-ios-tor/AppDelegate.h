852fe8e93923db3ef834c953003cbaa99eee98df
Ivan Doroshenko <ploddi@gmail.com>
2013-11-04 10:52:42 +0400

    Initial Commit

---
+++ prive-ios-tor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  prive-ios-tor
+//
+//  Created by Ivan Doroshenko on 11/4/13.
+//  Copyright (c) 2013 Prive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ prive-ios-tor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  prive-ios-tor
+//
+//  Created by Ivan Doroshenko on 11/4/13.
+//  Copyright (c) 2013 Prive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
